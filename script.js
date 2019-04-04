c1aa0e47a73ba66434e34aa007ebb9b8acee92ca
hit-11 <hiteshpeswani1111@gmail.com>
2019-04-04 16:06:04 +0530

    Highlight the active page in navigation menu. Jquery and Materialize added

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-function openNav() {
-    ham.classList.add('open');
-    document.getElementById("navigation").style.opacity = "1";
-    document.getElementById("navigation").style.width = "100%";
-}
-
-function closeNav() {
-    ham.classList.remove('open');
-    document.getElementById("navigation").style.width = "0%";
-    document.getElementById("navigation").style.opacity = "0";
-}
-
-var ham = document.getElementById("hamburger");
-ham.addEventListener('click', function() {
-    if (ham.classList != "open") {
-        openNav();
-    } else {
-        closeNav();
-    }
-});
-
-var overlayLinks = document.querySelector('.overlay-content');
-overlayLinks.addEventListener('click', closeNav);
-
 var changeWords = function() {
     var i = 0;
     var listOfWords = document.querySelectorAll('.words>span');
@@ -34,18 +10,46 @@ var changeWords = function() {
 }
 changeWords();
 
+var ham = document.getElementById("hamburger");
+ham.addEventListener('click', function() {
+    if (ham.classList != "open") {
+        openNav();
+    } else {
+        closeNav();
+    }
+});
+
+function openNav() {
+    ham.classList.add('open');
+    document.getElementById("navigation").style.opacity = "1";
+    document.getElementById("navigation").style.width = "100%";
+}
+
+function closeNav() {
+    ham.classList.remove('open');
+    document.getElementById("navigation").style.width = "0%";
+    document.getElementById("navigation").style.opacity = "0";
+}
+
+$('.overlay a').click(function() {
+    $('.overlay a').removeClass('activeLink');
+    $($(this)).addClass('activeLink');
+});
+var overlayLinks = document.querySelector('.overlay-content');
+overlayLinks.addEventListener('click', closeNav);
+
 var navLinks = document.querySelectorAll('.overlay a');
-var nl = Array.prototype.slice.call(navLinks);
-for (var i in nl) {
-    slideIn(nl, i);
-    slideOut(nl, i);
+var navArr = Array.prototype.slice.call(navLinks);
+for (var i in navArr) {
+    slideIn(navArr, i);
+    slideOut(navArr, i);
 }
 
 var skillLinks = document.querySelectorAll('#skill a');
-var sl = Array.prototype.slice.call(skillLinks);
-for (var i in sl) {
-    slideIn(sl, i);
-    slideOut(sl, i);
+var skillArr = Array.prototype.slice.call(skillLinks);
+for (var i in skillArr) {
+    slideIn(skillArr, i);
+    slideOut(skillArr, i);
 }
 
 function slideIn(arr, i) {
